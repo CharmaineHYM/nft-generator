ff7bc1857748a821cbfaa18eca36ff34dce64e47
charmainehym <charmaine.hym@gmail.com>
2022-02-21 10:58:37 +0800

    tidy up the code

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let showOption = function(e){
     showOption.classList.remove('disp-none')
 }
 
-headerBtn.addEventListener('click', showOption)
+headerBtn.addEventListener('click', showOption);
 
 // ------------------------------------------------------------------
 // input control
@@ -72,90 +72,57 @@ list.forEach(function(e){
 
 let generate = document.querySelector('.generate');
 
+// Create Random Image
+function randomImg(cls, arr){
+    let path = document.querySelectorAll(`.${cls} .custom_path`);
+    for(let i = 0; i< path.length; i++){
+        arr.push(path[i].value);
+    }
+
+    let el = new Image();
+    el.setAttribute("crossOrigin",'Anonymous');
+    let elnum = Math.floor(Math.random()*(path.length));
+    el.src = arr[elnum];
+
+    return el
+}
+
 let generator = function(e){
-    // Create Head Image
-    let headPath = document.querySelectorAll('.custom_head_wrapper .custom_path');
-    for(let i = 0; i< headPath.length; i++){
-        headImgArr.push(headPath[i].value);
-    }
 
-    let imghead = new Image();
-    imghead.setAttribute("crossOrigin",'Anonymous');
-    let imgheadnum = Math.floor(Math.random()*(headPath.length));
-    imghead.src = headImgArr[imgheadnum];
+    let imghead = randomImg('custom_head_wrapper', headImgArr);
+    imghead.onload = function(){
+            buildImg();
+        }
 
-    imghead.onload =  function(){
-        buildImg();
-    }
+    let imgeyes = randomImg('custom_eyes_wrapper', eyesImgArr);
+    imgeyes.onload = function(){
+            buildImg();
+        }
 
-    // Create Eyes Image
-    let eyesPath = document.querySelectorAll('.custom_eyes_wrapper .custom_path');
-    for(let i = 0; i< eyesPath.length; i++){
-        eyesImgArr.push(eyesPath[i].value);
-    }
+    
+    let imgeyeBrown = randomImg('custom_eyebrown_wrapper', eyeBrownImgArr);
+    imgeyeBrown.onload = function(){
+            buildImg();
+        }
 
-    let imgeyes = new Image();
-    imgeyes.setAttribute("crossOrigin",'Anonymous');
-    let imgeyesnum = Math.floor(Math.random()*(eyesPath.length));
-    imgeyes.src = eyesImgArr[imgeyesnum];
+    let imgnose = randomImg('custom_nose_wrapper', noseImgArr);
+    imgnose.onload = function(){
+            buildImg();
+        }
 
-    imgeyes.onload =  function(){
-        buildImg();
-    }
-
-    // Create Eyebrown Image
-    let eyeBrownPath = document.querySelectorAll('.custom_eyebrown_wrapper .custom_path');
-    for(let i = 0; i< eyeBrownPath.length; i++){
-        eyeBrownImgArr.push(eyeBrownPath[i].value);
-    }
-
-    let imgeyeBrown = new Image();
-    imgeyeBrown.setAttribute("crossOrigin",'Anonymous');
-    let imgeyeBrownnum = Math.floor(Math.random()*(eyeBrownPath.length));
-    imgeyeBrown.src = eyeBrownImgArr[imgeyeBrownnum];
-
-    imgeyeBrown.onload =  function(){
-        buildImg();
-    }
-
-    // Create Nose Image
-     let nosePath = document.querySelectorAll('.custom_nose_wrapper .custom_path');
-     for(let i = 0; i< nosePath.length; i++){
-         noseImgArr.push(nosePath[i].value);
-     }
- 
-     let imgnose = new Image();
-     imgnose.setAttribute("crossOrigin",'Anonymous');
-     let imgnosenum = Math.floor(Math.random()*(nosePath.length));
-     imgnose.src = noseImgArr[imgnosenum];
-
-     imgnose.onload =  function(){
-        buildImg();
-    }
-
-      // Create Mouth Image
-      let mouthPath = document.querySelectorAll('.custom_mouth_wrapper .custom_path');
-      for(let i = 0; i< mouthPath.length; i++){
-          mouthImgArr.push(mouthPath[i].value);
-      }
-  
-      let imgmouth = new Image();
-      imgmouth.setAttribute("crossOrigin",'Anonymous');
-      let imgmouthnum = Math.floor(Math.random()*(mouthPath.length));
-      imgmouth.src = mouthImgArr[imgmouthnum];
-
-      imgmouth.onload =  function(){
-        buildImg();
-      }
-
-    // create random background color
-
-    let r=Math.floor(Math.random() * (255 - 100 + 1) +100);
-    let g=Math.floor(Math.random() * (255 - 100 + 1) +100);
-    let b=Math.floor(Math.random() * (255 - 100 + 1) +100);
-    let bgColor = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`
+    let imgmouth = randomImg('custom_mouth_wrapper', mouthImgArr);
+    imgmouth.onload = function(){
+            buildImg();
+        }
 
     function buildImg(){
+         // create random background color
+        let r=Math.floor(Math.random() * (255 - 100 + 1) +100);
+        let g=Math.floor(Math.random() * (255 - 100 + 1) +100);
+        let b=Math.floor(Math.random() * (255 - 100 + 1) +100);
+        let bgColor = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`
+
+        // create canvas
         let ctx = canvas.getContext('2d');
         canvas.width=400;
         canvas.height=400;
